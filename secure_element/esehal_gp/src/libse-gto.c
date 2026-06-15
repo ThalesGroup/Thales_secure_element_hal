25b0813d313c7faa573a7c7940272681cce9f678
t0275573 <anna.agobian@thalesgroup.com>
2026-06-15 14:07:06 +0200

    fix log version AFTER ctx initialisation

--- secure_element/esehal_gp/src/libse-gto.c
+++ secure_element/esehal_gp/src/libse-gto.c
@@ -99,13 +99,13 @@ se_gto_new(struct se_gto_ctx **c)
     const char        *env;
     struct se_gto_ctx *ctx;
 
-    dbg("libse_gto_v%s", VERSION_LIBSE);
-    
     ctx = calloc(1, sizeof(struct se_gto_ctx));
     if (!ctx) {
         errno = ENOMEM;
         return -1;
     }
+
+    dbg("libse_gto_v%s", VERSION_LIBSE);
 
     isot1_init(&ctx->t1);
 
